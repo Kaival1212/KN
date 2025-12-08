4be7ff8c1c0644e7e792f73760a76f7a0077f784
Kaival <kaival225@gmail.com>
2025-12-08 14:39:16 +0000

    cookie bot

--- src/app/layout.js
+++ src/app/layout.js
@@ -75,8 +75,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script id="Cookiebot" strategy="beforeInteractive" src="https://consent.cookiebot.com/uc.js" data-cbid="646bffc5-94ca-49dd-9203-74e3ed314193" data-blockingmode="auto" type="text/javascript"></Script>
-
+        <Script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="646bffc5-94ca-49dd-9203-74e3ed314193" type="text/javascript" strategy="beforeInteractive"></Script>
       </head>
       <body
         className="antialiased"
