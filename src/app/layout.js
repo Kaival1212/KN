36a2e943a70c54f329c2cb7a05f2caa481296c24
Kaival <kaival225@gmail.com>
2025-12-08 14:21:41 +0000

    cookie bot

--- src/app/layout.js
+++ src/app/layout.js
@@ -75,6 +75,37 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <Script
+          id="Cookiebot"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="646bffc5-94ca-49dd-9203-74e3ed314193"
+          data-blockingmode="auto"
+          type="text/javascript"
+        ></Script>
+
+        {/* Google Consent Mode (ignored by Cookiebot) */}
+        <Script
+          data-cookieconsent="ignore"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag("consent", "default", {
+              ad_personalization: "denied",
+              ad_storage: "denied",
+              ad_user_data: "denied",
+              analytics_storage: "denied",
+              functionality_storage: "denied",
+              personalization_storage: "denied",
+              security_storage: "granted",
+              wait_for_update: 500,
+            });
+            gtag("set", "ads_data_redaction", true);
+            gtag("set", "url_passthrough", false);
+          `,
+          }}
+        ></Script>
+
       </head>
       <body
         className="antialiased"
